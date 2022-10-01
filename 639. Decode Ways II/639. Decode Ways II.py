4b1b33aa84918b4f1801e3179248bcfab1662f26
PrakNew <prakharnewatia97@gmail.com>
2022-10-01 13:55:35 +0530

    modified 639

--- 639. Decode Ways II/639. Decode Ways II.py
+++ 639. Decode Ways II/639. Decode Ways II.py
@@ -1,3 +1,47 @@
+# In order to understand the code you first need to go throught the Decode ways medium level question.
+# I am attaching my link to the solution there once you understand that problem you would easily understand this one too.
+# Decode Ways
+# Click on the above link
+
+# if you have understood the above solution then in below if we use DP we will end up in memory limit exceeded so in order to understand like what needs to be done we just need to modify it to 3 variables as in the below code and use the same logic but instead if * comes first then mulitiply by 9 and similar cases lik 1* means 18 and 2* means 6 and ** means 15 on similar basis you can also solve this problem
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        def decodes_ways(s):
+            dp = [0]*(10)
+            dp[0] = 1
+
+            if s[0]=='*':
+                dp[1] = 9
+            elif s[0]!='0':
+                dp[1] = 1
+            two,one=dp[0],dp[1]
+            new=0
+            for i in range(2,len(s)+1):
+                new=0
+                # think about 1-1 chars
+                if s[i-1] == '*':
+                    new = 9*one
+                    if s[i-2]=='1':
+                        new += 9*two
+                    elif s[i-2] == '2':
+                        new += 6*two
+                    elif s[i-2] == '*':
+                        new += (9*two + 6*two)
+                elif s[i-1] !='0':
+                    new += one
+
+                # think about 2-2 chars
+                if s[i-1]!='*':
+                    if s[i-2] == '1' or ( s[i-2] == '2' and s[i-1] < '7' ):
+                        new += two
+                    elif s[i-2]=='*':
+                        new += 2*two if s[i-1]<'7' else two
+                two,one=one,new
+            return new%(10**9+7)
+        return decodes_ways(s)
+
+
 class Solution:
     def numDecodings(self, s):
         n = len(s)
