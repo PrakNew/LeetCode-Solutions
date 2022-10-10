dd22dac8c98a49605a29d957a9a2ccbaca0ae721
PrakNew <prakharnewatia97@gmail.com>
2022-10-10 07:57:21 +0530

    130 modified

--- 130. Surrounded Regions/130. Surrounded Regions.py
+++ 130. Surrounded Regions/130. Surrounded Regions.py
@@ -1,3 +1,37 @@
+# The simple logic over here is to iterate over the boundaries and check whichever 'O' are connected to the boundary 'O'
+class Solution:
+    def solve(self, b: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n,m=len(b),len(b[0])
+        self.l=[["X"]*m for _ in range(n)]
+        visited=set()
+        def check(x,y):
+            if (x,y) in visited:
+                return 
+            if b[x][y]=='O':self.l[x][y]='O'
+            visited.add((x,y))
+            l=[(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+            for i,j in l:
+                if 0<=i<n and 0<=j<m and b[i][j]=='O':
+                    check(i,j)
+
+        for x in range(n):
+            if b[x][0] == 'O':
+                check(x,0)
+            if b[x][m-1]=='O':
+                check(x,m-1)
+        for x in range(m):
+            if b[0][x]=='O':
+                check(0,x)
+            if b[n-1][x]=='O':
+                check(n-1,x)
+        for x in range(n):
+            for y in range(m):
+                b[x][y]=self.l[x][y]
+
+
 import collections
 
 class Solution:
