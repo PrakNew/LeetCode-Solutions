4d640b77ec0add86d6b2513a845be19c2d7f9098
PrakNew <prakharnewatia97@gmail.com>
2022-10-15 22:39:22 +0530

    17 modified

--- 17. Letter Combinations of a Phone Number/17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number/17. Letter Combinations of a Phone Number.py
@@ -1,21 +1,38 @@
+from itertools import product
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:return []
+        d={
+            "2":list('abc'),
+            "3":list('def'),
+            "4":list('ghi'),
+            "5":list('jkl'),
+            "6":list('mno'),
+            "7":list('pqrs'),
+            "8":list('tuv'),
+            "9":list('wxyz')
+        }
+        l = [d[x] for x in digits]
+        z=list(product(*l))
+        z=list(map(lambda x:''.join(x),z))
+        return z
+
+
 class Solution:
     def letterCombinations(self, digits):
         
         letters = {2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9: 'wxyz'}
-        
+
         res = []
-        
+
         def util(digits, string = ''):
             if not digits:
                 res.append(string)
                 return
-            
+
             for ch in letters[int(digits[0])]:
                 util(digits[1:], string + ch)
-        
+
         util(digits)
-        
-        if len(res)==1:
-            return []
-                
-        return res
+
+        return [] if len(res)==1 else res
