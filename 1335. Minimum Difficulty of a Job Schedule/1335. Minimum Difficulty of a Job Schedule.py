9d3d02963c049eee45f00172e1736f7d4b8a899c
PrakNew <prakharnewatia97@gmail.com>
2022-10-16 06:49:37 +0530

    1335 modified

--- 1335. Minimum Difficulty of a Job Schedule/1335. Minimum Difficulty of a Job Schedule.py
+++ 1335. Minimum Difficulty of a Job Schedule/1335. Minimum Difficulty of a Job Schedule.py
@@ -4,7 +4,26 @@ Idea: DP
 Time complexity: O(n * d * n)
 Space complexity: O(n * d)
 """
+class Solution:
+    def minDifficulty(self, jobs: List[int], d: int) -> int:
+        if len(jobs)<d:return -1
+        n=len(jobs)
+        @cache
+        def check(ind,k):
+            if k==1:
+                try:
+                    return max(jobs[ind:])
+                except Exception:
+                    return inf
+            diff=0
+            mindiff=float('+inf')
+            for x in range(ind,n):
+                diff=max(diff,jobs[x])
+                mindiff=min(mindiff,diff+check(x+1,k-1))
+            return mindiff
 
+        return check(0,d)
+    
 import functools
 
 class Solution:
