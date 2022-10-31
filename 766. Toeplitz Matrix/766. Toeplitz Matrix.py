d99fd72869f5d24c055603ce9f7e427ddb6aadef
PrakNew <prakharnewatia97@gmail.com>
2022-10-31 06:21:04 +0530

    766 added

---
+++ 766. Toeplitz Matrix/766. Toeplitz Matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        n,m=len(matrix),len(matrix[0])
+        for x in range(1,n):
+            for y in range(1,m):
+                if matrix[x][y]!=matrix[x-1][y-1]:
+                    return False
+        return True
+        
