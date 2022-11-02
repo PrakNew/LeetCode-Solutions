22158028559bcf281958e1ca36c3274c6b978088
PrakNew <prakharnewatia97@gmail.com>
2022-11-02 06:47:38 +0530

    433 modified

--- 433. Minimum Genetic Mutation/433. Minimum Genetic Mutation.py
+++ 433. Minimum Genetic Mutation/433. Minimum Genetic Mutation.py
@@ -1,3 +1,29 @@
+#Simple BFS question 
+
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        def check(a,b):
+            c=0
+            for x in range(len(a)):
+                if a[x]!=b[x]:
+                    c+=1
+            return c
+        visited=set()
+        l=collections.deque([(start,0)])
+        flag=0
+        while l:
+            t,count=l.popleft()
+            if t==end:
+                return count
+            visited.add(t)
+            for x in bank:
+                if x not in visited and check(t,x)==1:
+                    l.append([x,count+1])
+        return -1
+
+
+
+
 import collections
 
 class Solution:
