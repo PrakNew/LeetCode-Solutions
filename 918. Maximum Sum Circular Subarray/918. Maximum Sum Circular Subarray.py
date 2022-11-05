daf92f47046f9ac38a2dd6d5eb20bc4bb251cd3c
PrakNew <prakharnewatia97@gmail.com>
2022-11-05 09:11:37 +0530

    918 modified

--- 918. Maximum Sum Circular Subarray/918. Maximum Sum Circular Subarray.py
+++ 918. Maximum Sum Circular Subarray/918. Maximum Sum Circular Subarray.py
@@ -1,3 +1,16 @@
+# Kadane algorithm modification with minsum in order to work with circular condition 
+class Solution:
+    def maxSubarraySumCircular(self, arr: List[int]) -> int:
+        if max(arr)<=0:
+            return max(arr)
+        currmax,maxsum,currmin,minsum=[arr[0]]*4
+        for x in range(1,len(arr)):
+            currmax=max(currmax+arr[x],arr[x])
+            maxsum=max(maxsum,currmax)
+            currmin=min(currmin+arr[x],arr[x])
+            minsum=min(currmin,minsum)
+        return max(maxsum,sum(arr)-minsum)
+
 class Solution:
     def maxSubarraySumCircular(self, A):
         
