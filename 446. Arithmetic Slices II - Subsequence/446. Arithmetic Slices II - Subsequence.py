48899d2560e67167de396c71388305f99841acdd
PrakNew <prakharnewatia97@gmail.com>
2022-11-29 09:21:50 +0530

    380 and 446 modified

--- 446. Arithmetic Slices II - Subsequence/446. Arithmetic Slices II - Subsequence.py
+++ 446. Arithmetic Slices II - Subsequence/446. Arithmetic Slices II - Subsequence.py
@@ -1,17 +1,17 @@
-from collections import defaultdict
+    from collections import defaultdict
 
-class Solution:
-    def numberOfArithmeticSlices(self, A):
-        n = len(A)
-        dp = [defaultdict(int) for _ in range(n)]
-        res = 0
-        
-        for i in range(n):
-            for j in range(i):
-                diff = A[i] - A[j]
-                dp[i][diff] += 1
-                if diff in dp[j]:
-                    dp[i][diff] += dp[j][diff]
-                    res += dp[j][diff]
-        
-        return res
+    class Solution:
+        def numberOfArithmeticSlices(self, A):
+            n = len(A)
+            dp = [defaultdict(int) for _ in range(n)]
+            res = 0
+            
+            for i in range(n):
+                for j in range(i):
+                    diff = A[i] - A[j]
+                    dp[i][diff] += 1
+                    if diff in dp[j]:
+                        dp[i][diff] += dp[j][diff]
+                        res += dp[j][diff]
+            
+            return res
