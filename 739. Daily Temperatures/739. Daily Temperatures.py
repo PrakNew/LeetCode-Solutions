c69454dddd6aaf7cd136ddfd111013d206fc17f5
PrakNew <prakharnewatia97@gmail.com>
2022-12-18 11:23:59 +0530

    739 modified

--- 739. Daily Temperatures/739. Daily Temperatures.py
+++ 739. Daily Temperatures/739. Daily Temperatures.py
@@ -1,3 +1,18 @@
+# Using Monotonic Stack it was implemented
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack=[]
+        final=[0]*len(temperatures)
+        for ind,x in enumerate(temperatures):
+            #print(stack)
+            if stack != [] and stack[-1][1] < x:
+                while stack!=[] and stack[-1][1]<x:
+                    top=stack.pop()
+                    final[top[0]]=ind-top[0]
+            stack.append([ind,x])
+        return final
+
+
 class Solution:
     def dailyTemperatures(self, T):
         n = len(T)
