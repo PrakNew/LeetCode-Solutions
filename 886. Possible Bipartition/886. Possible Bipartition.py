3827f4aab2efe20bc6d17ac44c1f228fdba2a181
Prakhar Newatia <40995798+PrakNew@users.noreply.github.com>
2022-12-21 22:53:41 +0530

    Update 886. Possible Bipartition.py

--- 886. Possible Bipartition/886. Possible Bipartition.py
+++ 886. Possible Bipartition/886. Possible Bipartition.py
@@ -1,3 +1,30 @@
+#Simple DFS with graph 
+from collections import defaultdict
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        def check(i,val):
+            if i in self.group and val!=self.group[i]:
+                return False
+            self.group[i]=val
+            if i not in self.visited:
+                self.visited.add(i)
+                for y in self.graph[i]:
+                    if not check(y,not val):return False
+            return True 
+        self.graph = defaultdict(list)
+        for x,y in dislikes:
+            self.graph[x].append(y)
+            self.graph[y].append(x)
+        self.visited=set()
+        self.group={}
+        for i in range(1,n+1):
+            if i not in self.visited:
+                if not check(i,False):
+                    return False
+        return True
+
+
+
 import collections
 
 class Solution:
