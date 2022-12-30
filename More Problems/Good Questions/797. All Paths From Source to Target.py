74fd3c2eedccf55ac5805a4701ecb318ac6cc3cf
Prakhar Newatia <prakhar.newatia@accrete.ai>
2022-12-30 12:15:55 +0530

    797 added and modified

--- More Problems/Good Questions/797. All Paths From Source to Target.py
+++ More Problems/Good Questions/797. All Paths From Source to Target.py
@@ -13,19 +13,3 @@ class Solution:
                 dfs(x,list(l)+[x])
         dfs(0,[0])
         return self.final
-class Solution:
-    def allPathsSourceTarget(self, graph):
-               
-        n = len(graph)
-        
-        def dfs(node, path, res):
-            if node==n-1:
-                res.append(path+[node])
-            
-            for nei in graph[node]:
-                dfs(nei, path + [node], res)
-        
-        res = []
-        dfs(0, [], res)
-        return res
-                
