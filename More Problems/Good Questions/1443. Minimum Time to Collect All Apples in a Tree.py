628e6ebae9f5d844e2009408b451d43328f0d8f5
Prakhar Newatia <prakhar.newatia@accrete.ai>
2023-01-11 12:39:08 +0530

    1443 added

---
+++ More Problems/Good Questions/1443. Minimum Time to Collect All Apples in a Tree.py
@@ -0,0 +1,19 @@
+# This is a tricky question which states that if we visit a node where there is apple then we have to do ans+2
+# and also the trick is we have to do ans+2 for all the parent nodes 
+
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        d=defaultdict(list)
+        visited = set()
+        for x,y in edges:
+            d[x].append(y)
+            d[y].append(x)
+        def dfs(n):
+            visited.add(n)
+            ans=0
+            for x in d[n]:
+                if x not in visited:
+                    ans+=dfs(x)
+            return 0 if ans==0 and hasApple[n]==False else ans+2
+
+        return max(0,dfs(0)-2)
