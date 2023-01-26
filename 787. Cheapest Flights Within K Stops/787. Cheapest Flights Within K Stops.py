0691a0f364808de1aab56454d01d4097320a4481
Prakhar Newatia <prakhar.newatia@accrete.ai>
2023-01-26 08:19:17 +0530

    787added

--- 787. Cheapest Flights Within K Stops/787. Cheapest Flights Within K Stops.py
+++ 787. Cheapest Flights Within K Stops/787. Cheapest Flights Within K Stops.py
@@ -1,3 +1,29 @@
+# Basic Dijikstra algorithm applied
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        d1=defaultdict(list)
+        for x,y,dist in flights:
+            d1[x].append((y,dist))
+        self.visited={}
+        def dfs(n,k1):
+            if n==dst:
+                return 0
+            if (n,k1) in self.visited:
+                return self.visited[(n,k1)]
+            if k1>k:
+                return float("inf")
+            distance=float("inf")
+            for y,dist in d1[n]:
+                distance=min(distance,dfs(y,k1+1)+dist) 
+            self.visited[(n,k1)]=distance
+            #print(self.visited)
+            return distance
+        final=dfs(src,0)
+        # print(self.visited)
+        return final if final!=float("inf") else -1           
+ # type: ignore
+
 import collections
 import heapq
 
