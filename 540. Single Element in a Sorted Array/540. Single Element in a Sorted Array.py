0b4bc1fd1fb63a20c97e6b9cd011d1e05fbf2bae
Prakhar Newatia <prakhar.newatia@accrete.ai>
2023-02-21 06:34:14 +0530

    540 added

--- 540. Single Element in a Sorted Array/540. Single Element in a Sorted Array.py
+++ 540. Single Element in a Sorted Array/540. Single Element in a Sorted Array.py
@@ -1,3 +1,17 @@
+#always find the even integer index and check if its next element is same or not and make the changes accordingly
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = 2 * ((lo + hi) // 4)
+            print(mid)
+            if nums[mid] == nums[mid+1]:
+                lo = mid+2
+            else:
+                hi = mid
+        return nums[lo]
+        return Counter(nums).most_common()[-1][0]
+
 class Solution:
     def singleNonDuplicate(self, nums):        
         
